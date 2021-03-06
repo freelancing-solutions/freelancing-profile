061b502e1f01293b2a43c8d94954cc884096950e
justice ndou <mobiusndou@gmail.com>
2021-03-06 16:57:39 +0200

    Corrected user-rest_api_test_case

--- client_api/main/tests/test_rest_user_api.py
+++ client_api/main/tests/test_rest_user_api.py
@@ -1,6 +1,6 @@
 import uuid, time
 from .. import db, create_app
-from flask import current_app
+from flask import current_app, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 
 def test_add_user_through_rest_api():
@@ -25,26 +25,11 @@ def test_add_user_through_rest_api():
     cell = "0764567890"
 
     with app.app_context():
+        # TODO- Create a test case by calling the User API Restful Api with the above data
+        data = jsonify({
+                "username":username,"email":email,"password":"mobius1234567",
+                "names":names,"surname":surname,"admin":admin,"img_link":img_link
+            })
 
-        user_model_instance = UserModel(uid=uid,username=username,email=email,password=password_hash,names=names,surname=surname,cell=cell,admin=admin,img_link=img_link)
-        try:
-            db.session.add(user_model_instance)
-            db.session.commit()
-        except Exception as error:
-            db.session.rollback()
-            db.session.commit()
-
-
-        assert isinstance(user_model_instance,UserModel), "User model not instantiating correctly"
-        assert user_model_instance.uid == uid, "UID not set correctly"
-        assert user_model_instance.username == username, "Username not set correctly"
-        assert user_model_instance.email == email, "Email not set Correctly"
-        assert user_model_instance.password == password_hash, "password hash not set correctly"
-        assert user_model_instance.names == names, "Names not set correctly"
-        assert user_model_instance.surname == surname, "Surname not set correctly"
-        assert user_model_instance.admin == admin, "Admin not set correctly"
-        assert user_model_instance.img_link == img_link, "img_link not set correctly"
-
-        user_model_recalled = UserModel.query.filter_by(uid=uid).first()
-        assert user_model_recalled == user_model_instance, "UserModel from database is being modified"
-
+        api_instance = UserAPI()
+        api_instance.dispatch_request()
