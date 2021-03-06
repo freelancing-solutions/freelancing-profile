fada5b16be133aa8a35527ace936c5e032205105
justice ndou <mobiusndou@gmail.com>
2021-03-06 12:25:36 +0200

    adding unittests
    started adding unit tests, and test cases

---
+++ client_api/main/tests/__init__.py
@@ -0,0 +1,14 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_blog_routes(self):
+        from ..blog import routes
+        pass
+
+
+
+
+if __name__ == '__main__':
+    # begin the unittest.main()
+    unittest.main()
