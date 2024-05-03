d76ae108b5b231ee2502ff846421512840ec96ff
mobius-crypt <mobiusndou@gmail.com>
2024-05-03 20:10:33 +0200

    Update run.py
    changed port number

--- client_api/run.py
+++ client_api/run.py
@@ -4,4 +4,4 @@ app = create_app()
 # Create app runs main.__init__()
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'], use_reloader=app.config['DEBUG'],
-            host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+            host='127.0.0.1', port=8086
